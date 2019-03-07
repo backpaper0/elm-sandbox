703556a05bae1152bfa0320babc4412769c6b821
Taichi Uragami <backpaper0@gmail.com>
2019-03-07 21:42:45 +0900

    WIP: ライフゲームっぽくなってきた

--- sandbox/src/LifeGame.elm
+++ sandbox/src/LifeGame.elm
@@ -4,7 +4,8 @@ import Array exposing (Array)
 import Browser
 import Svg exposing (..)
 import Svg.Attributes exposing (..)
-import Time
+import Time exposing (Posix)
+import Html exposing (Html)
 
 
 type alias Matrix a =
@@ -58,15 +59,74 @@ next a b =
         (a == 3)
 
 
-main =
-    let
-        size1 =
-            32
+type alias Model =
+    Matrix Bool
 
-        size2 =
+
+type Msg
+    = Tick Posix
+
+
+main =
+    Browser.element
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
+
+
+init : () -> ( Model, Cmd Msg )
+init () =
+    ( Matrix
+        (Array.fromList [ True, True, False, True, False, False, False, False, False, False, False, False, False, True, True, False, False, False, False, True, False, False, False, False, True, False, True, True, False, True, False, False, True, True, True, True, False, False, False, False, True, False, True, False, True, True, True, True, True, True, False, True, True, False, True, True, False, False, False, True, True, False, True, True, False, True, True, True, False, True, True, True, False, False, True, False, True, True, False, True, True, False, False, False, False, False, False, False, True, False, True, True, False, True, True, True, True, False, True, True, True, True, False, False, False, True, True, False, False, False, True, True, False, True, True, True, True, True, True, False, True, False, True, True, True, True, False, True, False, False, True, False, False, True, True, False, False, False, False, False, False, False, True, False, True, False, True, False, True, False, False, False, True, False, False, True, False, False, True, True, False, True, False, True, False, True, True, True, True, False, False, False, True, True, True, True, False, True, False, True, True, True, False, True, True, False, False, False, True, True, False, False, False, False, True, False, True, True, False, True, False, True, False, True, False, False, False, False, False, True, False, True, True, True, True, False, False, False, True, False, True, False, True, True, True, True, True, True, True, False, False, True, False, False, True, True, True, True, False, False, True, True, True, True, False, True, False, False, False, False, False, False, True, False, True, False, True, True, False, True, True, True, True, False, False, True, True, True, False, False, False, False, True, True, True, False, False, False, True, False, False, True, False, False, False, True, False, True, True, True, True, False, True, False, False, True, True, True, False, True, False, True, True, False, False, False, True, False, True, True, False, True, False, False, True, False, False, True, False, True, True, False, False, True, False, False, False, False, True, True, False, True, True, False, True, False, True, True, True, True, True, False, False, True, False, False, True, False, True, True, True, False, False, True, True, True, False, False, True, False, False, False, False, False, True, False, False, False, True, True, False, True, True, False, False, True, True, True, True, True, True, False, True, False, True, True, False, False, True, True, True, True, True, True, False, True, True, True, True, True, False, False, False, True, True, True, False, True, False, True, False, True, True, True, False, False, False, True, False, True, False, True, False, False, True, False, True, True, False, True, False, True, False, True, True, True, True, False, False, True, False, False, True, True, False, False, True, True, False, True, False, False, True, False, False, False, True, True, False, True, False, True, False, False, False, False, False, True, True, True, True, True, True, False, True, False, False, True, True, True, False, True, False, False, True, True, True, True, False, True, False, True, True, True, False, False, True, False, True, True, False, True, True, True, True, False, False, True, False, False, True, True, True, False, False, True, True, True, True, True, True, True, False, False, False, False, True, True, False, False, True, False, True, False, True, False, True, False, False, False, False, False, True, True, True, False, True, False, True, True, False, True, True, False, True, True, False, False, False, True, False, True, True, True, False, True, True, False, False, True, False, True, False, True, False, True, False, True, False, True, True, False, True, False, True, False, True, True, False, True, True, False, True, False, True, False, True, False, True, True, True, True, False, True, False, False, False, False, True, True, False, True, True, True, False, False, True, False, False, False, False, False, True, True, True, False, False, False, True, False, True, True, True, True, False, True, True, True, True, False, True, True, False, True, False, True, False, False, True, False, True, False, True, False, False, True, False, True, False, False, True, True, False, False, True, False, False, True, False, False, False, True, True, True, True, False, False, False, False, False, False, False, False, True, True, False, False, True, False, True, False, False, False, True, False, True, True, True, True, False, False, False, False, True, False, False, False, True, True, False, False, False, True, True, False, False, True, False, False, False, True, True, True, False, True, True, False, True, True, False, True, True, True, False, True, True, False, True, False, False, False, False, True, False, False, False, False, True, True, True, False, False, True, False, True, True, True, True, False, True, False, True, True, False, True, True, True, False, True, False, True, True, True, False, True, False, True, False, True, False, False, True, False, True, False, True, True, True, False, False, False, False, False, True, False, True, True, True, False, False, True, False, True, True, True, True, True, False, True, False, False, False, True, False, False, True, True, False, False, False, True, True, True, True, False, False, False, True, False, False, True, True, False, True, False, True, False, False, False, False, True, True, False, False, True, True, True, False, False, True, False, True, False, False, True, False, False, False, False, False, True, False, False, False, False, True, True, True, False, False, True, True, True, True, True, True, True, True, True, False, True, True, False, False, True, False, True, True, True, True, False, True, False, False, True, True, False, True, False, False, False, True, True, False, False, True, False, True, False, True, True, True, False, True, False, True, True, True, True, False, False, True, True, False, False, True, True, False, True, True, False, True, False, False, False, True, True, True, True, False, False, False, True, False, True, False, True, False, False, True, True, False, False, True, False, False, True, False, True, False, True, True, True, True, True, False, False, False, False, False, False, True, True, False, False, True, True, False, True, True, True, False, False, True, False, False, True, True, False, True, True, True, True, True, False, True, True, False, True, False, False, True, True, False, False, True, False, False, False, True, False, True, False, False, False, False, True, False, False, True, True, False, False, True ])
+        32
+    , Cmd.none
+    )
+
+
+view : Model -> Html Msg
+view model =
+    let
+        unit =
             20
 
         size3 =
-            size1 * size2
+            model.size * unit
+
+        fillColor a b =
+            get a b model
+                |> Maybe.map
+                    (\c ->
+                        if c then
+                            "rgb(100, 100, 100)"
+                        else
+                            "rgb(255, 255, 255)"
+                    )
+                |> Maybe.withDefault "rgb(255, 255, 255)"
+                |> fill
     in
-        svg [ width (String.fromInt size3), height (String.fromInt size3), viewBox ("0 0 " ++ String.fromInt size3 ++ " " ++ String.fromInt size3) ] (List.range 0 size1 |> List.map (\a -> List.range 0 size1 |> List.map (\b -> rect [ x (String.fromInt (a * size2)), y (String.fromInt (b * size2)), width (String.fromInt size2), height (String.fromInt size2), rx "10", ry "10" ] [])) |> List.concat)
+        Html.div []
+            [ svg
+                [ width (String.fromInt size3)
+                , height (String.fromInt size3)
+                , viewBox ("0 0 " ++ String.fromInt size3 ++ " " ++ String.fromInt size3)
+                ]
+                (List.range 0 model.size |> List.map (\a -> List.range 0 model.size |> List.map (\b -> rect [ x (String.fromInt (a * unit)), y (String.fromInt (b * unit)), width (String.fromInt unit), height (String.fromInt unit), rx "10", ry "10", fillColor a b ] [])) |> List.concat)
+            ]
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    let
+        f a b =
+            next (a |> Array.filter identity |> Array.length) b
+    in
+        case msg of
+            Tick _ ->
+                ( map f model, Cmd.none )
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Time.every 100 Tick
