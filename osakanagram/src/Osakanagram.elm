ee86f2d58d725fa73009e20a65ef26764362ca43
Taichi Uragami <backpaper0@gmail.com>
2019-06-01 06:28:37 +0900

    写真の部分を別の関数へ分離した

--- osakanagram/src/Osakanagram.elm
+++ osakanagram/src/Osakanagram.elm
@@ -33,23 +33,37 @@ main =
             [ style "width" "500px"
             , style "margin" "65px auto"
             ]
-            [ div
-                [ style "border" "1px solid silver"
-                , style "margin" "25px 0"
+            [ photoView { title = "肉重", image = "http://localhost:8080/images/54277395_270952053826359_6864398686538064173_n.jpg", fav = 1 }
+            , photoView { title = "カレー", image = "http://localhost:8080/images/51449439_382792795837276_3157664589089934568_n.jpg", fav = 0 }
+            ]
+        ]
+
+
+photoView { title, image, fav } =
+    div
+        [ style "border" "1px solid silver"
+        , style "margin" "25px 0"
+        ]
+        [ div [] [ img [ style "width" "100%", src image ] [] ]
+        , div [ style "padding" "0 5px" ] [ text title ]
+        , div [ style "padding" "0 5px" ]
+            [ button
+                [ style "cursor" "pointer"
+                , style "border" "0"
+                , style "fontSize" "large"
+                , style "backgroundColor" "transparent"
+                , style "color" <|
+                    if fav == 0 then
+                        "gray"
+                    else
+                        "PaleVioletRed"
                 ]
-                [ div [] [ img [ style "width" "100%", src "http://localhost:8080/images/51449439_382792795837276_3157664589089934568_n.jpg" ] [] ]
-                , div [ style "padding" "0 5px" ] [ text "カレー" ]
-                , div [ style "padding" "0 5px" ]
-                    [ button
-                        [ style "cursor" "pointer"
-                        , style "border" "0"
-                        , style "fontSize" "large"
-                        , style "backgroundColor" "transparent"
-                        , style "color" "gray"
-                        ]
-                        [ text "☆" ]
-                    , span [] [ text "いいね！", span [] [ text "0" ], text "件" ]
-                    ]
+                [ text <|
+                    if fav == 0 then
+                        "☆"
+                    else
+                        "★"
                 ]
+            , span [] [ text "いいね！", span [] [ text <| String.fromInt fav ], text "件" ]
             ]
         ]
